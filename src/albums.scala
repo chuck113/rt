acd32ac4d3c05f790b920dc2939e58b7dae2f86c
unknown <ck@.(none)>
2010-03-30 16:08:40 +0100

    improved ohhla importing, work on dsl - and removed stuff

--- src/albums.scala
+++
@@ -1,7 +0,0 @@
-import rt.dsl.AlbumSpec._
-
-buildIndexFrom("""C:\data\projects\rhyme-0.9\olhha""",
-  artist("Beastie Boys",
-    albums("Time to Get Ill", "Ill Communication","Paul's Botique", "Hello Nasty")
-  )
-)
